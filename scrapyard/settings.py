c5a2dc8c1ef56c15dd31976b322efa2280e39739
surya <suryafirefly2001@gmail.com>
2022-12-26 11:17:56 +0530

    updating sql-lite to MYSQL database

--- scrapyard/settings.py
+++ scrapyard/settings.py
@@ -78,8 +78,15 @@ WSGI_APPLICATION = 'scrapyard.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'creative_scrapyard',
+        'USER':'root',
+        'PASSWORD':'naperusurya2001',
+        'HOST':'127.0.0.1',
+        'PORT':'3306',
+        'OPTIONS':{
+            'init':"SET sql_mode ='STRICT_TRANS_TABLES'"
+        }
     }
 }
 
